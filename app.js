86ad3663fdb43a25078807107f77972ca0751119
Junaid Farooq <junaid@evercam.io>
2019-07-16 14:12:32 +0500

    Set search text to lowerCase.

--- app.js
+++ app.js
@@ -34,12 +34,12 @@ list.addEventListener("click", e => {
 const filterTodos = (term) => {
   Array
     .from(list.children)
-    .filter((todo) => !todo.textContent.includes(term))
+    .filter((todo) => !todo.textContent.toLowerCase.includes(term))
     .forEach((todo) => todo.classList.add("filtered"));
 
   Array
     .from(list.children)
-    .filter((todo) => todo.textContent.includes(term))
+    .filter((todo) => todo.textContent.toLowerCase.includes(term))
     .forEach((todo) => todo.classList.remove("filtered"));
 };
 
